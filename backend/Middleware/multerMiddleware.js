456798d6670a5a075b718e6413a5c541f3e00b15
SaileshKhattar21 <sailesh@drapcode.com>
2026-01-29 14:00:15 +0530

    Update multer configuration

--- backend/Middleware/multerMiddleware.js
+++ backend/Middleware/multerMiddleware.js
@@ -1,13 +1,89 @@
-import multer from "multer"
-import path from "path"
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
-    destination :"Uploads/",
-    filename : (req, file, callback)=>{
-        callback(null, Date.now()+path.extname(file.originalname));
+  destination: function (req, file, cb) {
+    const uploadType = req.body.uploadType;
+
+    if (!uploadType) {
+      return cb(new Error("uploadType is Required"));
     }
+
+    const userID = req.user?.id;
+    const username = req.body.username;
+
+    let basefolder;
+
+    if (userID) {
+      basefolder = `Uploads/${userID}`;
+    } else if (username) {
+      basefolder = `Uploads/temp/${username}`;
+    } else {
+      return cb(new Error("userId OR username is reuired"));
+    }
+
+    const folder =
+      uploadType === "profile"
+        ? `${basefolder}/profile`
+        : `${basefolder}/documents`;
+
+    fs.mkdirSync(folder, { recursive: true });
+
+    cb(null, folder);
+  },
+
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const filename = `${Date.now()}${ext}`;
+    cb(null, filename);
+  },
+});
+
+const fileFilter = (req, file, cb){
+    const uploadType =req.body.uploadType;
+
+    if(uploadType === "profile"){
+        if(!file.mimetype.startwith("image/")){
+            return cb(new Eroor("Only images are allowed"))
+        }
+    }
+
+    if(uploadType === "Document"){
+        const allowed = [
+      "application/pdf",
+      "application/msword",
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    ];
+
+    if(!allowed.includes(file.mimetypes)){
+        return cb(new Error("Invalid Document type"))
+    }
+    }
+
+    cb(null, true)
+}
+
+
+
+const moveTempProfile = (username, userId) => {
+  const tempPath = path.join("uploads", "temp", username);
+  const finalPath = path.join("uploads", "users", userId.toString());
+
+  if (!fs.existsSync(tempPath)) {
+    return; // nothing to move
+  }
+
+  fs.mkdirSync(path.dirname(finalPath), { recursive: true });
+
+  fs.renameSync(tempPath, finalPath);
+};
+
+export default moveTempProfile;
+
+
+export const uploaad = multer({
+    storage, 
+    fileFilter,
+    limits : {fileSize : 10*1024*1024} 
 })
-
-const upload = multer({storage})
-
-export default upload;
--- backend/Models/Resource.js
+++ backend/Models/Resource.js
@@ -1,10 +1,32 @@
 import mongoose from "mongoose";
+import User from "./Users.js";
+import Topics from "./Topics";
 
-const ResourceSchema = new mongoose.Schema ({
-    description : {type : String, required :true}, 
-    createdBy : {type : mongoose.Schema.Types.ObjectId, ref : User}, 
-    Topic : {type : mongoose.Schema.Types.ObjectId, ref : Topics},
-}, {timestamps : true})
+const ResourceSchema = new mongoose.Schema(
+  {
+    description: { type: String, required: true },
+    type: {
+      type: String,
+      enum: ["Document", "Link"],
+      required: true,
+    },
 
+    url: {
+      type: String,
+      required: function () {
+        return this.type === "Link";
+      },
+    },
+    content: {
+      type: String,
+      required: function () {
+        return this.type === "Document";
+      },
+    },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: User },
+    Topic: { type: mongoose.Schema.Types.ObjectId, ref: Topics },
+  },
+  { timestamps: true },
+);
 
-export default mongoose.model("Resource",ResourceSchema)
+export default mongoose.model("Resource", ResourceSchema);
